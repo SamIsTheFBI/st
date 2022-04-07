2fb4cea18c4f564cd89d78751d593b82bea01e3d
SamIsTheRetard <mrqube001@gmail.com>
2022-04-07 14:55:57 +0530

    changed JetBrainsMono Nerd Font
    glyphs no longer look diminished with Nerd Font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:size=10";    //"Liberation Mono:pixelsize=12:antialias=true:autohint=true"
+static char *font = "JetBrainsMono Nerd Font:style:medium:size=10"; //"Liberation Mono:pixelsize=12:antialias=true:autohint=true"
 static int borderpx = 2;
 
 /*
