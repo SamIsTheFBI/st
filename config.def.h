198d3cee991caa874dc9687013b37fb9c9502f86
SamIsTheFBI <mrqube001@gmail.com>
2023-06-24 12:01:56 +0530

    font changes

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:style:medium:size=10";
+static char *font = "JetBrainsMono Nerd Font:style:bold:size=10";
 static int borderpx = 2;
 
 /*
